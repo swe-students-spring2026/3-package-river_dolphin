b5c4ac4d493709f4306388920472082a27e813a5
samaythe1 <sd5135@nyu.edu>
2026-03-29 12:49:39 -0400

    Update CLI to use wag() function

--- src/pydog/__main__.py
+++ src/pydog/__main__.py
@@ -1,5 +1,8 @@
+from .core import wag
+
 def main():
-    print("Welcome to pydog!")
+    print("Welcome to pydog!\n")
+    print(wag("run via python -m pydog"))
 
 if __name__ == "__main__":
     main()
