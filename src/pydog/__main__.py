d4c61ac458419a1d580d3e0cc0b3cf69f8d8801a
murnbn <te2137@nyu.edu>
2026-03-30 20:16:56 -0400

    added speak in main

--- src/pydog/__main__.py
+++ src/pydog/__main__.py
@@ -1,4 +1,4 @@
-from .core import wag, bark
+from .core import speak, bark, wag
 import argparse
 # use arg parse to create a command line interface for the pydog package.
 def main():
@@ -6,13 +6,18 @@ def main():
     parser.add_argument("--bark", type=int, help="enter a number to specify the number of barks")
     parser.add_argument("--wag", type=str, help="enter a message for the dog to wag")
 
+    parser.add_argument("--speak-message", type=str, help="message for speak")
+    parser.add_argument("--speak-mood", type=str, help="mood for speak (happy, sleepy, angry)")
+
     
     args = parser.parse_args()
     
-    if args.bark:
+    if args.bark is not None:
         print(bark(args.bark))
-    elif args.wag:
+    elif args.wag is not None:
         print(wag(args.wag))
+    elif args.speak_message is not None and args.speak_mood is not None:
+        print(speak(args.speak_message, args.speak_mood))
     else:
         parser.print_help()
 
